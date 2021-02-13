dac0915c58c88928fc2060ed383ae3c48e48d926
merima98 <merimaceranic98@gmail.com>
2021-02-13 22:47:50 +0100

    Removing all content, because of needs of adding changes to project

--- src/features/components/Home.js
+++ src/features/components/Home.js
@@ -1,30 +1,7 @@
 import React from "react";
-import styled from "styled-components";
-import { BREAKPOINTS } from "../../constants";
-
-import Portofolio from "./Portofolio";
-
-const Wrapper = styled.div`
-  min-height: 100vh;
-  background-color: #d01527;
-`;
-
-const Container = styled.div`
-  padding: 450px 20px;
-  @media (min-width: ${BREAKPOINTS.SMALL_DEVICES}) {
-    max-width: 3000px;
-    margin: auto;
-  }
-`;
 
 function Home() {
-  return (
-    <Wrapper>
-      <Container>
-        <Portofolio />
-      </Container>
-    </Wrapper>
-  );
+  return <div></div>;
 }
 
 export default Home;
--- src/features/components/Portofolio.js
+++ src/features/components/Portofolio.js
@@ -1,23 +1,7 @@
 import React from "react";
-import styled from "styled-components";
-
-import Header from "./Header";
-
-const Wrapper = styled.div`
-  /* background-color: red; */
-  width: 100%;
-  height: 120rem;
-  background-color: #333333;
-  border-radius: 8px;
-  box-shadow: 5px 10px 20px 15px #82030f;
-`;
 
 function Portofolio() {
-  return (
-    <Wrapper>
-      <Header />
-    </Wrapper>
-  );
+  return <div></div>;
 }
 
 export default Portofolio;
