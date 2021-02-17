22832ef2e7f23368f7c394a799e0eafa5eab6ef7
merima98 <merimaceranic98@gmail.com>
2021-02-17 21:23:39 +0100

    Adding global style and removing .css and .test.js files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./App.css";
 import { Route, Switch } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./features/components/Home";
 import Contact from "./features/components/Contact";
 import UserInformation from "./features/components/UserInformation";
+import GlobalStyle from "./globalStyles";
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/contact" component={Contact} />
